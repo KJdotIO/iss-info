0ee42ab4ff8f07e10ac6769cc560b688bde69f33
KJdotIO <kj@compscilib.com>
2023-03-08 21:59:52 +0000

    fun api app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import { React, useState, useEffect } from 'react';
+import Card from './Card';
+
+
+import { FaArrowDown } from 'react-icons/fa';
 
 function App() {
+  var today = new Date();
+  var formattedDate = today.toLocaleDateString("en-GB");
+  console.log(formattedDate);
+
+
+  const [issData, setissData] = useState([])
+  const [miles, setMiles] = useState(0)
+  const [altitude, setAltitude] = useState(0)
+
+  
+  const toMiles = (velocity) => {
+    return(
+      velocity * 0.621371
+    )
+  }
+
+  
+  useEffect(() => {
+    let intervalId;
+    
+    const fetchData = () => {
+      fetch('https://api.wheretheiss.at/v1/satellites/25544')
+      .then(response => response.json())
+      .then(data => {
+        toMiles()
+        setissData(data)
+
+        const miles = toMiles(data.velocity)
+        setMiles(miles)
+      })
+    }
+
+    fetchData();
+
+    intervalId = setInterval(fetchData, 2000);
+
+    return () => {
+      clearInterval(intervalId);
+    }
+  }, [miles]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='bg-[#1f1f1f] min-h-screen text-white flex flex-col justify-center items-center'>
+      
+      <h1 className='my-[40px] mx-[30px] text-5xl text-center font-semibold'>International Space <br /> Station info.</h1>
+      <Card
+      name={issData.name}
+       latitude={issData.latitude}
+       longitude={issData.longitude}
+       velocity={Math.floor(miles)}
+       visibility={issData.visibility}  
+       date={formattedDate}
+       units={'miles'}
+       />
+        
+        <div><FaArrowDown className=' text-5xl my-[20px]' /></div>
+
+        <footer>
+          <h1 className='font-extrabold'>This was built using <a href='https://wheretheiss.at/w/developer' className=' text-blue-400'>This API</a></h1>
+        </footer>
     </div>
+    </>
   );
 }
 
---
+++ src/Card.js
@@ -0,0 +1,85 @@
+import React from "react";
+import '../src/media/iss-black.jpg'
+
+import iss from '../src/media/iss-black.jpg'
+
+const Card = ({name, latitude, longitude, velocity, visibility, date, units, altitude}) => {
+    
+    return(
+        <>
+        <div className="
+         flex
+         justify-center
+         items-center
+         bg-[#292929]
+         w-[600px]
+         p-[30px]
+          rounded-[20px]
+          max-w-[90%]
+          box-border
+        ">
+            <div
+            >
+                <div>
+                    <img
+                    alt="iss"
+                    src={iss}
+                    style={{
+                        maxWidth: '100%',
+                        borderRadius: '20px'
+                    }}
+                    />
+                </div>
+
+                <hr className="my-[30px] opacity-50" />
+
+                <div
+                className=" text-[1rem]"
+                >
+                    <div className=" opacity-50 text-2xl font-extrabold flex justify-between">
+                    <h1>Satalie:</h1>
+                    <h1>{name}</h1>
+
+                    </div>
+
+                    <div className="font-semibold">
+
+                        <div className="flex justify-between">
+                            <p>Latitude:</p>
+                            <p>{latitude}</p>
+                        </div>
+                        <div className="flex justify-between">
+                            <p>Longitude:</p>
+                            <p>{longitude}</p>
+                        </div>
+                        <div className="flex justify-between">
+                            <p>Visibility:</p>
+                            <p>{visibility}</p>
+                        </div>
+                        <div className="flex justify-between">
+                            <p>Current date:</p>
+                            <p>{date}</p>
+                        </div>
+                        {/* <div className="flex justify-between">
+                            <p>Current altitude:</p>
+                            <p>{altitude}</p>
+                        </div> */}
+                        <div className="flex justify-between">
+                            <p>MPH:</p>
+                            <p>{velocity}</p>
+                        </div>
+                        <div className="flex justify-between">
+                            <p>Units:</p>
+                            <p>{units}</p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Card;
