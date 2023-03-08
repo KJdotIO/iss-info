89dfc2c6d96cfea1e88b8554ea0ae35785078a02
KJdotIO <kj@compscilib.com>
2023-03-08 22:10:59 +0000

    bro

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function App() {
 
         <footer className='p-[20px] text-center'>
           <h1 className=''>This was built using <a href='https://wheretheiss.at/w/developer' className=' text-blue-400'>This API</a>.</h1>
-          <h1 className=''>The FETCH method is updated every 3 seconds because otherwise it throws a fit and throws a 429 error.</h1>
+          <h1 className=''>The FETCH method is updated every 2 seconds because otherwise it throws a fit and throws a 429 error.</h1>
         </footer>
     </div>
     </>
